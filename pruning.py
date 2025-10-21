1a6203afe5532853657b3df1fd93fad651d01e6e
PK3408 <158382328+PK3408@users.noreply.github.com>
2025-10-21 00:02:34 -0400

    Add files via upload

--- pruning.py
+++ pruning.py
@@ -3,10 +3,26 @@ from torch import nn
 import torch.nn.utils.prune as prune
 import torch.nn.functional as F
 import os
+from llama.model import Transformer, ModelArgs, FeedForward
+
+# Step 1: Define model
+model_args = ModelArgs(
+    dim=4096,
+    n_layers=32,
+    n_heads=32,
+    n_kv_heads=None,
+    vocab_size=32000,
+    max_batch_size=32,
+    max_seq_len=2048,
+)
+
+model = Transformer(model_args)
 
 var = input("please enter file path:")
-model = torch.load(var)
-prune.l1_unstructured(model, name = "weight", amount=.5)
+file = torch.load(var)
+model.load_state_dict(file)
+model.eval()
+prune.l1_unstructured(model.layers[0].feed_forward.w1, name = "weight", amount=.5)
 torch.save(model, "pruning/pruned"+var)
 
 
