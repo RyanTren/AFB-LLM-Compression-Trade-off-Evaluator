ffd7d03c021a2cc7d00fbfd77718f3dd89a7ce12
ryan <concepting@protonmail.com>
2025-10-25 21:12:08 -0400

    [test]

--- src/scripts/run_inference.py
+++ src/scripts/run_inference.py
@@ -12,7 +12,7 @@ os.environ["HF_HUB_DISABLE_TELEMETRY"] = "1"
 
 # --- Configuration ---
 BASE_MODEL = "codeparrot/codeparrot-small"  # Must match training model!
-ADAPTER_PATH = "./lora_out_codeparrot_PROPER/checkpoint-epoch1-step624"  # Point to specific checkpoint
+ADAPTER_PATH = "./lora_out_codeparrot_10_25_25/checkpoint-epoch1-step624"  # Point to specific checkpoint
 PROMPTS_PATH = "../src/data/code_prompts.json"
 MAX_NEW_TOKENS = 128
 TEMPERATURE = 0.7
