ba7620675cf8e550000adef91003fbe3d1135a69
omgaldi <142700045+omgaldi@users.noreply.github.com>
2025-11-09 20:01:30 -0500

    Update codebleu_dashboard_app.py

--- codebleu_dashboard_app.py
+++ codebleu_dashboard_app.py
@@ -6,7 +6,7 @@ import streamlit as st
 
 # ---------- Config ----------
 API_URL_DEFAULT = "http://localhost:8510/generate"
-CODEBLEU_DIR = str(Path("~/CodeXGLUE/Code-Code/code-to-code-trans/evaluator/CodeBLEU").expanduser())
+CODEBLEU_DIR = str(Path("~/mnt/cst/gpu4/sperry46/p10-t1llmcomp/CodeXGLUE/Code-Code/code-to-code-trans/evaluator/CodeBLEU").expanduser())
 CALC_SCRIPT = str(Path(CODEBLEU_DIR) / "calc_code_bleu.py")
 SPLIT_TOKEN = "<END_OF_SNIPPET>"
 # ----------------------------
